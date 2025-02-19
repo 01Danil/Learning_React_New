8e2dc41bca29ab28ce4fcb4905aa800b8efe3740
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-02-19 10:15:21 +0000

    Add page 88

--- index.js
+++ index.js
@@ -21,11 +21,66 @@
 </section>;
 */
 
+/*
 const dish = React.createElement("h1", null, "Baked Salmon");
 const dessert = React.createElement("h2", null, "Coconut Cream Pie");
 
 ReactDOM.render([dish, dessert], document.getElementById("root"));
+*/
 
+/*
+const list = React.createElement(
+  "ul",
+  null,
+  React.createElement("li", null, "2 lb salmon"),
+  React.createElement("li", null, "5 sprigs fresh rosemary"),
+  React.createElement("li", null, "2 tablespoons olive oil"),
+  React.createElement("li", null, "2 small lemons"),
+  React.createElement("li", null, "1 teaspoon kosher salt"),
+  React.createElement("li", null, "4 cloves of chopped garlic")
+);
+
+console.log(list);
+*/
+
+/*
+React.createElement(
+  "section",
+  { id: "baked-salmon" },
+  React.createElement("h1", null, "Baked Salmon"),
+  React.createElement(
+    "ul",
+    { className: "ingredients" },
+    React.createElement("li", null, "2 lb salmon"),
+    React.createElement("li", null, "5 sprigs fresh rosemary"),
+    React.createElement("li", null, "2 tablespoons olive oil"),
+    React.createElement("li", null, "2 small lemons"),
+    React.createElement("li", null, "1 teaspoon kosher salt"),
+    React.createElement("li", null, "4 cloves of chopped garlic")
+  ),
+  React.createElement(
+    "section",
+    { className: "instruction" },
+    React.createElement("h2", null, "Cooking Instructions"),
+    React.createElement("p", null, "Preheat the oven to 375 degrees."),
+    React.createElement("p", null, "Lightly coat aluminum foil with oil."),
+    React.createElement("p", null, "Place salmon on foil."),
+    React.createElement(
+      "p",
+      null,
+      "Cover with rosemary, sliced lemons, chopped garlic."
+    ),
+    React.createElement(
+      "p",
+      null,
+      "Bake for 15-20 minutes until cooked through."
+    ),
+    React.createElement("p", null, "Remove from oven.")
+  )
+);
+*/
+
+/*
 React.createElement(
   "ul",
   null,
@@ -36,3 +91,75 @@ React.createElement(
   React.createElement("li", null, "1 teaspoon kosher salt"),
   React.createElement("li", null, "4 cloves of chopped garlic")
 );
+*/
+
+/*
+const items = [
+  "2 lb salmon",
+  "5 sprigs fresh rosemary",
+  "2 tablespoons olive oil",
+  "2 small lemons",
+  "1 teaspoon kosher salt",
+  "4 cloves of chopped garlic",
+];
+
+React.createElement(
+  "ul",
+  { className: "ingredients" },
+  items.map((ingredient, i) =>
+    React.createElement("li", { key: i }, ingredient)
+  )
+);
+*/
+
+/*
+function ingredientList() {
+  return React.createElement(
+    "ul",
+    { className: "ingredients" },
+    React.createElement("li", null, "1 cup unsalted butter"),
+    React.createElement("li", null, "1 cup crunchy peanut butter"),
+    React.createElement("li", null, "1 cup brown sugar"),
+    React.createElement("li", null, "1 cup white sugar"),
+    React.createElement("li", null, "2 eggs"),
+    React.createElement("li", null, "2.5 cups all purpose flour"),
+    React.createElement("li", null, "1 teaspoon baking powder"),
+    React.createElement("li", null, "0.5 teaspoon salt")
+  );
+}
+
+ReactDOM.render(
+  React.createElement(ingredientList, null, null),
+  document.getElementById("root")
+);
+*/
+
+/*
+const secretIngredients = [
+  "1 cup unsalted butter",
+  "1 cup crunchy peanut butter",
+  "1 cup brown sugar",
+  "1 cup white sugar",
+  "2 eggs",
+  "2.5 cups all purpose flour",
+  "1 teaspoon baking powder",
+  "0.5 teaspoon salt",
+];
+
+function IngredientsList(props) {
+  return React.createElement(
+    "ul",
+    { className: "ingredients" },
+    props.items.map((ingredient, i) =>
+      React.createElement("li", { key: i }, ingredient)
+    )
+  );
+}
+
+ReactDOM.render(
+  React.createElement(IngredientsList, { items: secretIngredients }, null),
+  document.getElementById("root")
+);
+*/
+
+
