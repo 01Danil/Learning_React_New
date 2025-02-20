67dbf7bf629385ce5ff910bf703196a4ee87c67f
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-02-20 13:25:55 +0000

    Add page 103

--- index.js
+++ index.js
@@ -1,210 +1,36 @@
 /*
-const date = [
-  {
-    name: "Baked Salmon",
-    ingredients: [
-      { name: "Salmon", amount: 1, measurement: "l lb" },
-      { name: "Pine Nuts", amount: 1, measurement: "cup" },
-      { name: "Butter Lettuce", amount: 2, measurement: "cups" },
-      { name: "Yellow Squash", amount: 1, measurement: "med" },
-      { name: "Olive Oil", amount: 0.5, measurement: "cup" },
-      { name: "Garlic", amount: 3, measurement: "cloves" },
-    ],
-    steps: [
-      "Preheat the oven to 350 degrees.",
-      "Spread the olive oil around a glass baking dish.",
-      "Add the yellow squash and place in the oven for 30 mins.",
-      "Add the salmon, garlic, and pine nuts to the dish.",
-      "Bake for 15 minutes.",
-      "Remove from oven. Add the lettuce and serve.",
-    ],
-  },
-  {
-    name: "Fish Tacos",
-    ingredients: [
-      { name: "Whitefish", amount: 1, measurement: "l lb" },
-      { name: "Cheese", amount: 1, measurement: "cup" },
-      { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
-      { name: "Tomatoes", amount: 2, measurement: "large" },
-      { name: "Tortillas", amount: 3, measurement: "med" },
-    ],
-    steps: [
-      "Cook the fish on the grill until cooked through.",
-      "Place the fish on the 3 tortillas.",
-      "Top them with lettuce, tomatoes, and cheese.",
-    ],
-  },
-];
+function Cat({ name }) {
+  return <h1>The cat`s name is {name}</h1>
+}
+ReactDOM.render(<Cat name="Jungle" />, document.getElementById("root"));
 */
 
 /*
-// Данные, массив объектов Recipes
-const data = [ ... ];
-
-// Функциональный компонент, предназначенный для Recipes
-function Recipe (props) {
-	...
+function Cat({ name }) {
+  return <h1>The cat`s name is {name}</h1>
+	<p>He`s good</p>
 }
-
-// Функциональный компонент, предназначенный для Menu
-function Menu (props) {
-	...
-	}
-
-	// Вызов ReactDOM.render для рендеринга Menu в текущей DOM-модели
-	reactDOM.render(
-		<Menu recipes={date} title="Delicious Recipes"/>,
-		document.getElementById("root")
-	);
-	*/
+ReactDOM.render(<Cat name="Jungle" />, document.getElementById("root"));
+*/
 
 /*
-function Menu(props) {
+function Cat({ name }) {
   return (
-    <article>
-      <header>
-        <h1>{props.title}</h1>
-      </header>
-    </article>
+    <React.Fragment>
+      <h1>The cat`s name is {name}</h1>
+      <p>He`s good.</p>
+    </React.Fragment>
   );
 }
 */
 
-/*
-<div className="recipes">
-  {props.recipes.map((recipe, i) => (
-    <Recipe
-      key={i}
-      name={recipe.name}
-      ingredients={recipe.ingredients}
-      steps={recipe.steps}
-    />
-  ))}
-</div>;
-*/
-
-/*
-{
-  props.recipes.map((recipe, i) => <Recipe key={i} {...recipe} />);
-}
-*/
-
-/*
-function Menu({ title, props }) {
+function Cat({ name }) {
   return (
-    <article>
-      <header>
-        <h1>{title}</h1>
-      </header>
-      <div className="recipes">
-        {recipes.map((recipe, i) => (
-          <Recipe key={i} {...recipe} />
-        ))}
-      </div>
-    </article>
+    <>
+      <h1>The cat`s name is {name}</h1>
+      <p>He`s good.</p>
+    </>
   );
 }
-
-function Recipe({ name, ingredients, steps }) {
-  return (
-    <section id={name.toLowerCase().replace(/ /g, "-")}>
-      <h1>{name}</h1>
-      <ul className="ingredients">
-        {ingredients.map((ingredient, i) => (
-          <li key={i}>{ingredient.name}</li>
-        ))}
-      </ul>
-      <section className="instructions">
-        <h2>Cooking Instructions</h2>
-        {steps.map((step, i) => (
-          <p key={i}>{step}</p>
-        ))}
-      </section>
-    </section>
-  );
-}
-
-<div className="recipes">
-  {recipes.map((recipe, i) => (
-    <Recipe key={i} {...recipe} />
-  ))}
-</div>;
-
-ReactDOM.render(
-  <Menu recipes={data} title="Delicious Recipes" />,
-  document.getElementById("root")
-);
-*/
-
-const data = [
-  {
-    name: "Baked Salmon",
-    ingredients: [
-      { name: "Salmon", amount: 1, measurement: "l lb" },
-      { name: "Pine Nuts", amount: 1, measurement: "cup" },
-      { name: "Butter Lettuce", amount: 2, measurement: "cups" },
-      { name: "Yellow Squash", amount: 1, measurement: "med" },
-      { name: "Olive Oil", amount: 0.5, measurement: "cup" },
-      { name: "Garlic", amount: 3, measurement: "cloves" },
-    ],
-    steps: [
-      "Preheat the oven to 350 degrees.",
-      "Spread the olive oil around a glass baking dish.",
-      "Add the yellow squash and place in the oven for 30 mins.",
-      "Add the salmon, garlic, and pine nuts to the dish.",
-      "Bake for 15 minutes.",
-      "Remove from oven. Add the lettuce and serve.",
-    ],
-  },
-  {
-    name: "Fish Tacos",
-    ingredients: [
-      { name: "Whitefish", amount: 1, measurement: "l lb" },
-      { name: "Cheese", amount: 1, measurement: "cup" },
-      { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
-      { name: "Tomatoes", amount: 2, measurement: "large" },
-      { name: "Tortillas", amount: 3, measurement: "med" },
-    ],
-    steps: [
-      "Cook the fish on the grill until hot.",
-      "Place the fish on the 3 tortillas.",
-      "Top them with lettuce, tomatoes, and cheese.",
-    ],
-  },
-];
-function Recipe({ name, ingredients, steps }) {
-  return (
-    <section id={name.toLowerCase().replace(/ /g, "-")}>
-      <h1>{name}</h1>
-      <ul className="ingredients">
-        {ingredients.map((ingredient, i) => (
-          <li key={i}>{ingredient.name}</li>
-        ))}
-      </ul>
-      <section className="instructions">
-        <h2>Cooking Instructions</h2>
-        {steps.map((step, i) => (
-          <p key={i}>{step}</p>
-        ))}
-      </section>
-    </section>
-  );
-}
-function Menu({ title, recipes }) {
-  return (
-    <article>
-      <header>
-        <h1>{title}</h1>
-      </header>
-      <div className="recipes">
-        {recipes.map((recipe, i) => (
-          <Recipe key={i} {...recipe} />
-        ))}
-      </div>
-    </article>
-  );
-}
-ReactDOM.render(
-  <Menu recipes={data} title="Delicious Recipes" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Cat name="Jungle" />, 
+document.getElementById("root"));
